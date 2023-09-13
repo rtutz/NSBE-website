3d540ef9c15c98e4a496d1f6fb39424a95899f9d
rtutz <ronaldjohn.tutor@gmail.com>
2023-09-13 11:13:17 -0400

    initial implementation of landing page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,22 +1,33 @@
 import Lottie from "lottie-react";
 
 import data from '../assets/lottie_geographic.json';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationPin } from "@fortawesome/free-solid-svg-icons";
 
 function Homepage() {
     const style = {
-        height: 500,
+        height: 2000,
       };
 
     return (
         <>
         {/* Added empty div to push down homepage and respect header*/}
-        <div className="h-20"/>
+       
 
 
-        <div id="#homepage" className="homepage flex justify-between">
-             <div className="textBanner text-4xl font-sans font-bold">
-                NSBEHacks
-             </div>
+        <div id="#homepage" className="homepage flex justify-between items-center mt-20 mx-10">
+            <div className="flex flex-col">
+                <div className="text-8xl font-sans font-bold text-palette-200">
+                    NSBEHacks
+                </div>
+                <div className="mt-3 text-xl font-sans font-bold">
+                    The first student-run <span className="text-palette-100 text-xl font-sans font-bold">black hackathon</span> in the GTA
+                </div>
+                <div className="flex flex-col mt-5 first-letter:font-sans text-palette-400 font-semibold">
+                    <span className="font-thin tracking-wider">Toronto, Ontario</span>
+                    <span className="font-thin tracking-wider mt-1">October 25-27, <span className="text-palette-100 font-thin tracking-wider">2023</span></span>
+                </div>
+            </div>
             <div> 
                 <Lottie animationData={data} style={style}/>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,20 @@ export default {
     fontFamily: {
       'sans': ['Calibre', 'ui-sans-serif', 'system-ui'],
     },
-    colors: {
-      'palette': {
-        100: '#64ffda',
-        200: '#e6f1ff',
-        300: '#ccd6f6',
-        400: '#a8b2d1',
-        500: '#8892b0',
-        600: '#233554',
-        700: '#112240',
-        800: '#0a192f'
+    extend: {
+      colors: {
+        'palette': {
+          100: '#64ffda',
+          200: '#e6f1ff',
+          300: '#ccd6f6',
+          400: '#a8b2d1',
+          500: '#8892b0',
+          600: '#233554',
+          700: '#112240',
+          800: '#0a192f'
+        },
       },
     },
-    extend: {},
   },
   plugins: [],
 }
