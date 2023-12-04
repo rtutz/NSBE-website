79bc9e155ef5aae7625f618b4ed386f1a89dfdb6
rtutz <ronaldjohn.tutor@gmail.com>
2023-12-04 01:25:21 -0500

    added minor changes to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,7 @@
 import { TypeAnimation } from 'react-type-animation';
-import "../App.css"
-
 function About() {
     return (
-      <div className="flex h-screen flex-col items-center justify-center">
+      <div className="flex h-screen flex-col items-center justify-center About">
             <TypeAnimation
                     wrapper={"div"}
                     sequence={['About Us', 500]}
@@ -13,15 +11,15 @@ function About() {
                     repeat={0}
                     className="type"
               />
-  
+
             <div className='mx-40 text-lg text-center mt-20 leading-loose'>
             The National Society of Black Engineers (NSBE) is one of the largest student-governed organizations based in the United States. NSBE, founded in 1975, supports and promotes the aspirations of collegiate and pre-collegiate students and technical professionals in engineering and technology.
 
 With more than 600 chapters and more than 24,000 active members in the U.S. and abroad, NSBE’s mission is “to increase the number of culturally responsible black engineers who excel academically, succeed professionally and positively impact the community."
             </div>
-          
-          </div>
 
+      
+          </div>
 
     )
   }
