330edfac75c947987475c8d8a84745414da168d0
rtutz <ronaldjohn.tutor@gmail.com>
2023-12-04 00:38:21 -0500

    initialized about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,27 +3,27 @@ import "../App.css"
 
 function About() {
     return (
-        <div className="flex flex-col justify-center mt-20 mx-10">
-            <div id="about-header">
+      <div className="flex h-screen flex-col items-center justify-center">
             <TypeAnimation
-            wrapper={"div"}
-            sequence={['About Us', 500]}
-            cursor={false}
-            speed={1}
-            style={{ fontSize: '3rem' }}
-            repeat={0}
-            className="type"
-            />
-            </div>
-            <div id="about-content">
-                <p>
-                The National Society of Black Engineers (NSBE) is one of the largest student-governed organizations based in the United States. NSBE, founded in 1975, supports and promotes the aspirations of collegiate and pre-collegiate students and technical professionals in engineering and technology.
+                    wrapper={"div"}
+                    sequence={['About Us', 500]}
+                    cursor={false}
+                    speed={1}
+                    style={{ fontSize: '2rem' }}
+                    repeat={0}
+                    className="type"
+              />
+  
+            <div className='mx-40 text-lg text-center mt-20 leading-loose'>
+            The National Society of Black Engineers (NSBE) is one of the largest student-governed organizations based in the United States. NSBE, founded in 1975, supports and promotes the aspirations of collegiate and pre-collegiate students and technical professionals in engineering and technology.
 
-                With more than 600 chapters and more than 24,000 active members in the U.S. and abroad, NSBE’s mission is “to increase the number of culturally responsible black engineers who excel academically, succeed professionally and positively impact the community“
-                </p>
+With more than 600 chapters and more than 24,000 active members in the U.S. and abroad, NSBE’s mission is “to increase the number of culturally responsible black engineers who excel academically, succeed professionally and positively impact the community."
             </div>
-        </div>
+          
+          </div>
+
+
     )
-}
+  }
 
 export default About;
