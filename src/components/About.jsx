e2800ce942f55801f7f6e372b682ce5158377171
rtutz <ronaldjohn.tutor@gmail.com>
2023-12-03 21:57:19 -0500

    before adding typing animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,17 @@
 function About() {
     return (
-        <section id="about">
-            <h1 className="">About</h1>
-        </section>
+        <div className="flex flex-col justify-center mt-20 mx-10">
+            <div id="about-header">
+                <h1 className="font-sans font-bold text-5xl mb-10 tracking-normal">ABOUT US</h1>
+            </div>
+            <div id="about-content">
+                <p>
+                The National Society of Black Engineers (NSBE) is one of the largest student-governed organizations based in the United States. NSBE, founded in 1975, supports and promotes the aspirations of collegiate and pre-collegiate students and technical professionals in engineering and technology.
+
+                With more than 600 chapters and more than 24,000 active members in the U.S. and abroad, NSBE’s mission is “to increase the number of culturally responsible black engineers who excel academically, succeed professionally and positively impact the community“
+                </p>
+            </div>
+        </div>
     )
 }
 
