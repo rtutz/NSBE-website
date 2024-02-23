48dc1ec7d221b9b6c86d24fb3f643dbf996b0365
rtutz <ronaldjohn.tutor@gmail.com>
2024-02-22 19:13:47 -0500

    fix FAQ again

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -78,7 +78,7 @@ function Faq({setActiveLink}) {
             <FaqCard question={"Who can participate?"} answer={"The event is open for everybody, regardless of racial identity and educational background."}/>
             <FaqCard question={"Is there a registration fee?"} answer={"No, there is no registration fee. Additionally, participants will enjoy complimentary food, including snacks, and there will be designated spaces for breaks and naps."}/>
             <FaqCard question={"What if I don't have a team?"} answer={"No problem at all if you don't have a team yet! We've set up dedicated events and communication servers to help you find like-minded teammates. Feel free to join in, connect with others, and let's make the team-building process a breeze. We're here to ensure everyone has a smooth and enjoyable experience throughout the hackathon."}/>
-            <FaqCard question={"What do I need to bring?"} answer={"Just bring your laptop and charger, and you're good to go! If you're up for it, a sleeping bag is highly suggested. Feel free to bring along anything else that makes you comfortable and ready to hack."}/>
+            <FaqCard question={"What do I need to bring?"} answer={"Just bring your laptop and charger, and you're good to go! Feel free to bring along anything else that makes you comfortable and ready to hack."}/>
             </div>
             
 
